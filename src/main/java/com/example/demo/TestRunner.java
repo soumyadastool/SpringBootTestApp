e3e82e1e6b5fddd82e4fa9dafb17b79c4b570f5e
tl@gmail.com <Dell@Dell-PC>
2021-06-16 05:34:26 -0400

    File Updated

---
+++ src/main/java/com/example/demo/TestRunner.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class TestRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Welcome To Kdots");
+
+	}
+
+}
